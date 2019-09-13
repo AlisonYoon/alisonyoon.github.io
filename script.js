5d0444ad3be32472475747f28150dbbe92c1afab
AlisonYoon <politescarlett1@gmail.com>
2019-09-13 16:59:12 +0100

    it only works with id for some reason

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let apiUrl = "https://api.github.com/users/AlisonYoon/repos?sort=updated";
 
 
-var printHere = document.getElementsByClassName('repo-link')
+var printHere = document.getElementById('testId')
 var obj;
 
 fetch(apiUrl)
@@ -9,3 +9,4 @@ fetch(apiUrl)
     .then(data => obj = data)
     .then(() => console.log(obj))
     .then(()=> printHere.innerHTML = obj[0]["name"])
+    .then(()=> printHere.href = obj[0]["html_url"])
