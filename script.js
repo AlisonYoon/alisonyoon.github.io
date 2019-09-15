4b3e41d7bbdc2ac1c2672b674b4c5318e1d0479b
AlisonYoon <politescarlett1@gmail.com>
2019-09-15 16:15:03 +0100

    assigned href, but with baseURL in front. needs to be fixed

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ let apiUrl = "https://api.github.com/users/AlisonYoon/repos?sort=updated";
 var printHere = document.getElementById('testId')
 var obj;
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => obj = data)
-    .then(() => console.log(obj))
-    .then(()=> printHere.innerHTML = obj[0]["name"])
-    .then(()=> printHere.href = obj[0]["html_url"])
+let getApiResponse = (apiUrl) => {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => obj = data)
+        .then(
+            function(obj) {
+                for(let i = 0; i < 6; i++) {
+                    printHere.innerHTML += "<a href='' target='_blank' class='repo_link'>" + obj[i]["name"] + "</a><br/>"
+                    let repoLinkHref = document.querySelectorAll('.repo_link')
+                    repoLinkHref[i].href += obj[i]["html_url"]
+                    // for(let j = 0; j < 6; j++) {
+                    //     let repoLinkHref = document.querySelectorAll('.repo_link')
+                    //     repoLinkHref[j].href += obj[j]["html_url"]
+                    // }
+                }
+            })
+    return obj;
+}
+
+getApiResponse(apiUrl);
