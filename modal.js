b9e1aa1ce8def0c148056d2d7573fad3f490dbcc
AlisonYoon <politescarlett1@gmail.com>
2019-09-28 20:16:22 +0100

    addecd github link in the footer of the modal

--- modal.js
+++ modal.js
@@ -4,7 +4,6 @@ const isVisible = "is-visible";
 
 for (const el of openEls) {
     el.addEventListener("click", function() {
-        console.log('clicked');
         const modalId = this.dataset.open;
         document.getElementById(modalId).classList.add(isVisible);
     });
